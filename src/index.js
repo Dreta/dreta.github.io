d42e43815fe4b35b90261474a1d9433fee8a0e79
Dreta <yangmain3@gmail.com>
2021-03-28 21:09:23 +0800

    Add secondary email

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const discord = document.getElementById("discord");
 
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("email").href = "mai" + "lto:hel" + "lo@dr" + "eta.dev";
+    document.getElementById("email-2").href = "mai" + "lto:yan" + "gma" + "in3@" + "gma" + "il.c" + "om";
 
     discord.onclick = async () => {
         await navigator.clipboard.writeText("Dre" + "t" + "a#6" + "65" + "5");
