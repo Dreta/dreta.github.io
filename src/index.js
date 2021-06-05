35e3ac8ee20e9aaf831f6377ddd95835002fc7aa
Gabriel "Dreta" Leen <hello@dreta.dev>
2021-06-05 11:05:59 +0800

    feat: add dynamic 3d transform

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2021 Dreta, all rights reserved.
 
 const discord = document.getElementById("discord");
+const prim = document.getElementById("prim-container");
 
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("email").href = "mai" + "lto:hel" + "lo@dr" + "eta.dev";
@@ -12,5 +13,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
     discord.onkeyup = async () => {
         await navigator.clipboard.writeText("Dre" + "t" + "a#6" + "65" + "5");
+    };
+
+    document.querySelector("body").onpointermove = (e) => {
+        const width = window.innerWidth / 2;
+        let transform = "";
+
+        if (e.clientX > width) {
+            transform = `rotateY(-${(e.clientX - width) / width * 30}deg) `;
+        } else if (e.clientX <= width) {
+            transform = `rotateY(${(width - e.clientX) / width * 30}deg) `;
+        }
+
+        const height = window.innerHeight / 2;
+        if (e.clientY > height) {
+            transform += `rotateX(${(e.clientY - width) / width * 30}deg)`;
+        } else if (e.clientY <= height) {
+            transform += `rotateX(-${(width - e.clientY) / width * 30}deg)`;
+        }
+        prim.style.transform = transform;
     };
 });
